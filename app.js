8358755d806bf9c0d50fe0946f0921fd20ee26d4
muhammad noman <mohammadhasan772@gmail.com>
2016-09-24 15:17:17 +0500

    making table

---
+++ app.js
@@ -0,0 +1,31 @@
+var a = prompt('Enter Your Table No:')
+a = parseInt(a);
+var b = (a * '1');
+document.write(b + '<br>');
+
+var b = (a * '2');
+document.write(b + '<br>');
+
+var b = (a * '3');
+document.write(b + '<br>');
+
+var b = (a * '4');
+document.write(b + '<br>');
+
+var b = (a * '5');
+document.write(b + '<br>');
+
+var b = (a * '6');
+document.write(b + '<br>');
+
+var b = (a * '7');
+document.write(b + '<br>');
+
+var b = (a * '8');
+document.write(b + '<br>');
+
+var b = (a * '9');
+document.write(b + '<br>');
+
+var b = (a * '10');
+document.write(b + '<br>');
